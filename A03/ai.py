db15bce539fd0a605e164ea98cda498792a4c932
William Fox <wfox96@gmail.com>
2018-03-02 12:02:21 -0800

    Very Basic Ai, doesn't chose moves

--- A03/ai.py
+++ A03/ai.py
@@ -3,11 +3,26 @@ ai - search & strategy module
 
 implement a concrete Strategy class and AlphaBetaSearch
 """
+import abstractstrategy
+from checkerboard import *
 
 
-class Strategy:
-    pass
+class Strategy(abstractstrategy.Strategy):
 
+    def play(self, board) -> (CheckerBoard, tuple):
+        pass
+
+    def utility(self, board: CheckerBoard) -> int:
+        __king_weight = 5
+        kings = board.get_kingsN()
+        pawns = board.get_pawnsN()
+
+        red = __king_weight * kings[0] + pawns[0]
+        black = __king_weight * kings[1] + pawns[1]
+        if board.playeridx(self.maxplayer) is 0:
+            return red - black
+        else:
+            return black - red
 
 class AlphaBetaSearch:
     """AlphaBetaSearch
@@ -25,7 +40,6 @@ class AlphaBetaSearch:
 
     def __init__(self, strategy, maxplayer, minplayer, maxplies=3,
                  verbose=False):
-
         """"AlphaBetaSearch - Initialize a class capable of alphabeta search
         strategy - implementation of AbstractStrategy class
         maxplayer - name of player that will maximize the utility function
@@ -35,7 +49,6 @@ class AlphaBetaSearch:
         """
 
     def alphabeta(self, state):
-
         """alphbeta(state) - Run an alphabeta search from the current
        state. Returns best action.
        """
