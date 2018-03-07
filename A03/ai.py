f7eb29ba4c2f94526f1c84f773ae000da65b4c57
Tom Paulus <tom@tompaulus.com>
2018-03-06 22:57:26 -0800

    Improved AI Utility

--- A03/ai.py
+++ A03/ai.py
@@ -3,19 +3,20 @@ ai - search & strategy module
 
 implement a concrete Strategy class and AlphaBetaSearch
 """
+import sys
+
 import abstractstrategy
 from checkerboard import *
-import sys
 
 
 class Node:
-
     def __init__(self, state: CheckerBoard, super_parent=None):
         self.state = state
         self.super_parent = super_parent
 
 
 class Strategy(abstractstrategy.Strategy):
+    __utility_weights = [100, 10, 1]
 
     def play(self, board: CheckerBoard) -> (CheckerBoard, tuple):
         search = AlphaBetaSearch(self, self.maxplayer, self.minplayer, self.maxplies)
@@ -24,17 +25,47 @@ class Strategy(abstractstrategy.Strategy):
             return board, None
         return board.move(action), action
 
+    # Based off of https://github.com/billjeffries/jsCheckersAI/blob/master/scripts/checkers-engine.js
     def utility(self, board: CheckerBoard) -> int:
-        __king_weight = 3
-        kings = board.get_kingsN()
-        pawns = board.get_pawnsN()
+        player_pieces = 0
+        player_kings = 0
+        player_pos_sum = 0
 
-        red = __king_weight * kings[0] + pawns[0]
-        black = __king_weight * kings[1] + pawns[1]
-        if board.playeridx(self.maxplayer) is 0:
-            return red - black
-        else:
-            return black - red
+        opponent_pieces = 0
+        opponent_kings = 0
+        opponent_pos_sum = 0
+
+        for (_r, _c, piece) in board:
+            # Find player index and piece type
+            (playerId, kingP) = board.identifypiece(piece)
+            if playerId == board.playeridx(self.maxplayer):
+                # This Player
+                player_pieces += 1
+                if kingP:
+                    player_kings += 1
+                player_pos_sum += self.evaluate_position(board, self.maxplayer, _r, _c)
+
+            else:
+                # Opponent
+                opponent_pieces += 1
+                if kingP:
+                    opponent_kings += 1
+                opponent_pos_sum += self.evaluate_position(board, self.minplayer, _r, _c)
+
+        player_features = [player_pieces, player_kings, player_pos_sum]
+        player_score = sum(feature * weight for (feature, weight) in zip(player_features, self.__utility_weights))
+
+        opponent_features = [opponent_pieces, opponent_kings, opponent_pos_sum]
+        opponent_score = sum(feature * weight for (feature, weight) in zip(opponent_features, self.__utility_weights))
+
+        # print("Utility Calculation - R: {}  B: {}".format(player_score, opponent_score))
+
+        return player_score - opponent_score
+
+    @staticmethod
+    def evaluate_position(board: CheckerBoard, player, x, y) -> int:
+        is_edge = (x == 0 or x == 7 or y == 0 or y == 7)
+        return (5 if is_edge else 3) + board.disttoking(player, y)
 
 
 class AlphaBetaSearch:
