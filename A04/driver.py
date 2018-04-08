ee776cdbebdd814d56b596258722aca2c8c608e8
Tom Paulus <tom@tompaulus.com>
2018-04-08 11:04:08 -0700

    Implemented A4 Driver

--- A04/driver.py
+++ A04/driver.py
@@ -1,8 +1,23 @@
-from csp_lib.sudoku import (Sudoku, easy1, harder1)
-from constraint_prop import AC3
-from csp_lib.backtrack_util import mrv
-from backtrack import backtracking_search
+from copy import deepcopy
 
-for puzzle in [easy1, harder1]:
-    s = Sudoku(puzzle)  # construct a Sudoku problem
-    print(s.display(s.infer_assignment()))
+from backtrack import backtracking_search
+from constraint_prop import AC3
+from csp_lib.sudoku import (Sudoku, easy1, harder1)
+
+if __name__ == "__main__":
+    for puzzle in [easy1, harder1]:
+        s = Sudoku(puzzle)  # construct a Sudoku problem
+        print(s.display(s.infer_assignment()))
+
+        if AC3(deepcopy(s)):
+            print("Solved via AC3")
+            print(s.display(s.infer_assignment()))
+
+        else:
+            print("Could not solve via AC3")
+
+            if backtracking_search(s):
+                print("Solved via Back Track")
+                print(s.display(s.infer_assignment()))
+            else:
+                print("Could not be solved")
