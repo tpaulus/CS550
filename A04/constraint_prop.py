88dd057fc5a94a8d664bf4ddb68a1c1abf901f49
William Fox <wfox96@gmail.com>
2018-04-08 11:14:06 -0700

    It doesn't crash

--- A04/constraint_prop.py
+++ A04/constraint_prop.py
@@ -1,16 +1,37 @@
 '''
 Constraint propagation
 '''
+from csp_lib.csp import CSP
 
 
-def AC3(csp, queue=None, removals=None):
+def AC3(csp: CSP, queue=None, removals=None):
     """AC3 constraint propagation
     
     """
+    queue = [(i, j) for i in csp.domains for j in csp.neighbors[i]]
+
+    while queue:
+        i, j = queue.pop()  # Cell in Puzzle, Set Neighbors of Cell
+        if revise(csp, i, j):
+            if csp.domains[i] is None:
+                return False
+            for x in csp.neighbors[i]:
+                if x not in csp.neighbors[j]:
+                    queue.append((x, i))
+        return True
 
     # Hints:
     # Remember that:
     #    csp.variables is a list of variables
-    #    cps.neighbors[x] is the neighbors of variable x
+    #    cps.j[x] is the j of variable x
 
-    raise NotImplemented
+
+def revise(csp: CSP, i, j):
+    revised = False
+    for x in csp.domains[i]:
+        if not any([csp.constraints(i, x, j, y) for y in csp.domains[j]]):
+            if type(csp.domains) is not list:
+                csp.domains[i] = list(csp.domains[i])
+            csp.domains[i].remove(x)
+            revised = True
+    return revised
