d667d5484a0b196ebf76867e7b674e746daeb36a
William Fox <wfox96@gmail.com>
2018-04-08 17:01:34 -0700

    Improved Driver (check notes)
    Added is Solved
    
    Changed logic for printing success/fail for AC3

--- A04/driver.py
+++ A04/driver.py
@@ -4,6 +4,14 @@ from backtrack import backtracking_search
 from constraint_prop import AC3
 from csp_lib.sudoku import (Sudoku, easy1, harder1)
 
+
+def is_solved(sudoku: Sudoku):
+    for var in sudoku.curr_domains:
+        if len(sudoku.curr_domains[var]) is not 1:
+            return False
+    return True
+
+
 if __name__ == "__main__":
     # for puzzle in [easy1, harder1]:
     for puzzle in [easy1]:
@@ -11,15 +19,17 @@ if __name__ == "__main__":
         print(s.display(s.infer_assignment()))
 
         ac3_s = deepcopy(s)
-        if AC3(ac3_s):
+        AC3(ac3_s)
+        if is_solved(ac3_s):
             print("Solved via AC3")
             print(ac3_s.display(ac3_s.infer_assignment()))
-
         else:
             print("Could not solve via AC3")
-
-            if backtracking_search(s):
+            print(ac3_s.display(ac3_s.infer_assignment()))
+            print("Trying to solve via Back Track")
+            # backtracking_search(s) # Non Implemented
+            if is_solved(s):
                 print("Solved via Back Track")
                 print(s.display(s.infer_assignment()))
             else:
-                print("Could not be solved")
+                print("Puzzle could not be solved")
