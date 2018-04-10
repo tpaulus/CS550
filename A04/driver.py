15b3a3dcdfbb4daf9af36d8f023da6ec77fe2eb5
William Fox <wfox96@gmail.com>
2018-04-10 16:07:18 -0700

    updated inference function

--- A04/driver.py
+++ A04/driver.py
@@ -1,5 +1,6 @@
 from backtrack import backtracking_search
 from constraint_prop import AC3
+from csp_lib.backtrack_util import mrv, mac
 from csp_lib.sudoku import (Sudoku, easy1, harder1)
 
 
@@ -12,7 +13,7 @@ def is_solved(sudoku: Sudoku):
 
 if __name__ == "__main__":
     for puzzle in [easy1, harder1]:
-    # for puzzle in [easy1]:
+        # for puzzle in [easy1]:
         s = Sudoku(puzzle)  # construct a Sudoku problem
         print("New Sudoku Puzzle")
         s.display(s.infer_assignment())
@@ -24,11 +25,10 @@ if __name__ == "__main__":
             print("Could not solve via AC3: Current Puzzle")
             s.display(s.infer_assignment())
             print("Trying to solve via Back Track")
-            backtracking_search(s)
+            backtracking_search(s, inference=mac)
             if is_solved(s):
                 print("Solved via Back Track")
                 s.display(s.infer_assignment())
             else:
                 print("Puzzle could not be solved: Current State")
                 s.display(s.infer_assignment())
-
