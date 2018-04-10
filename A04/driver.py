0fe08ab7c7939b9d49f00591fd2a2c5a70902a3b
William Fox <wfox96@gmail.com>
2018-04-10 15:25:55 -0700

    improved driver

--- A04/driver.py
+++ A04/driver.py
@@ -1,5 +1,3 @@
-from copy import deepcopy
-
 from backtrack import backtracking_search
 from constraint_prop import AC3
 from csp_lib.sudoku import (Sudoku, easy1, harder1)
@@ -13,23 +11,24 @@ def is_solved(sudoku: Sudoku):
 
 
 if __name__ == "__main__":
-    # for puzzle in [easy1, harder1]:
-    for puzzle in [easy1]:
+    for puzzle in [easy1, harder1]:
+    # for puzzle in [easy1]:
         s = Sudoku(puzzle)  # construct a Sudoku problem
-        print(s.display(s.infer_assignment()))
-
-        ac3_s = deepcopy(s)
-        AC3(ac3_s)
-        if is_solved(ac3_s):
+        print("New Sudoku Puzzle")
+        s.display(s.infer_assignment())
+        AC3(s)
+        if is_solved(s):
             print("Solved via AC3")
-            print(ac3_s.display(ac3_s.infer_assignment()))
+            s.display(s.infer_assignment())
         else:
-            print("Could not solve via AC3")
-            print(ac3_s.display(ac3_s.infer_assignment()))
+            print("Could not solve via AC3: Current Puzzle")
+            s.display(s.infer_assignment())
             print("Trying to solve via Back Track")
-            # backtracking_search(s) # Non Implemented
+            backtracking_search(s)
             if is_solved(s):
                 print("Solved via Back Track")
-                print(s.display(s.infer_assignment()))
+                s.display(s.infer_assignment())
             else:
-                print("Puzzle could not be solved")
+                print("Puzzle could not be solved: Current State")
+                s.display(s.infer_assignment())
+
