1af26e3117118e63e3f7b05f3483c49deb31309f
William Fox <wfox96@gmail.com>
2018-04-24 15:37:40 -0700

    implemented shuffle_data and typed learn param

--- A05/driver.py
+++ A05/driver.py
@@ -4,15 +4,18 @@ Created on Apr 15, 2018
 @author: mroch
 '''
 
-from ml_lib.learning import (DataSet,
-                             DecisionTreeLearner, NeuralNetLearner)
+from ml_lib.learning import (DataSet, DecisionTreeLearner, NeuralNetLearner)
 from std_cv import cross_validation
 from random import shuffle
 
 from copy import deepcopy
 
 
-def learn(dataset):
+def shuffle_data(dataset: DataSet):
+    shuffle(dataset.examples)
+
+
+def learn(dataset: DataSet):
     raise NotImplemented
 
 
